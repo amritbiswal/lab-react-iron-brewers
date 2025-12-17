1e136dc4948d64c872d73d64731155cc3e304f96
Amrit Biswal <amrit.k.biswal@gmail.com>
2025-12-17 18:01:09 +0100

    Lab updated - All Test cases are passed now

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -52,7 +52,7 @@ function AddBeerPage() {
       image_url: imageUrl,
       first_brewed: firstBrewed,
       brewers_tips: brewersTips,
-      attenuation_level: Number(attenuationLevel),
+      attenuation_level: attenuationLevel,
       contributed_by: contributedBy,
     };
     try {
